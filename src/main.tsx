4edd614aac549b44dc74709a36b70a3e733ec4d7
Temi-pinheiro <temitopepinheiro@gmail.com>
2023-11-02 04:52:31 +0100

    remove dev toolds

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Router, RouterProvider } from '@tanstack/react-router';
-import { TanStackRouterDevtools, queryClient, routeTree } from './routes';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient, routeTree } from './routes';
 
 const router = new Router({
   routeTree,
@@ -22,8 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools />
-      <TanStackRouterDevtools router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
